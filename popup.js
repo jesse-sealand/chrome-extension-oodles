b151501832bccaed2caef40878cb60d892315e26
Jesse Sealand <jesse.sealand@outlook.com>
2025-04-13 18:30:05 -0400

    Popup
    - Added table headers

--- popup.js
+++ popup.js
@@ -2,10 +2,30 @@ window.addEventListener('DOMContentLoaded', () => {
     chrome.tabs.query({windowId: chrome.windows.WINDOW_ID_CURRENT}, (tabs) => {
 
         var table = document.getElementById("rankTable");
+
+        // Create table header
+        var header = table.createTHead();
+        var row = header.insertRow(0);
+
+        var headerCols = ["Url","Tab ID"];
+
+        for (let i = 0; i < headerCols.length; i++) {
+
+            //var cell = row.insertCell(i);
+            //cell.innerHTML = `${headerCols[i]}`;
+
+            let th = document.createElement("th");
+            let text = document.createTextNode(`${headerCols[i]}`);
+            th.appendChild(text);
+            row.appendChild(th);
+
+        };
+
+        // Create table records
         for (let i = 0; i < tabs.length; i++) {
     
             // Create new record for this table
-            var row = table.insertRow(0);
+            var row = table.insertRow(1);
     
             // Create new cells for this record
             var cell1 = row.insertCell(0);
